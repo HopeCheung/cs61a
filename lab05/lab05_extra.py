b37f22bddbc2e03107414b4fd128498ac948a0e0
HopeCheung <34972841+HopeCheung@users.noreply.github.com>
2018-11-30 22:13:28 -0500

    Add files via upload

---
+++ lab05/lab05_extra.py
@@ -0,0 +1,192 @@
+""" Optional questions for Lab 05 """
+
+from lab05 import *
+
+# Shakespeare and Dictionaries
+def build_successors_table(tokens):
+    """Return a dictionary: keys are words; values are lists of successors.
+
+    >>> text = ['We', 'came', 'to', 'investigate', ',', 'catch', 'bad', 'guys', 'and', 'to', 'eat', 'pie', '.']
+    >>> table = build_successors_table(text)
+    >>> sorted(table)
+    [',', '.', 'We', 'and', 'bad', 'came', 'catch', 'eat', 'guys', 'investigate', 'pie', 'to']
+    >>> table['to']
+    ['investigate', 'eat']
+    >>> table['pie']
+    ['.']
+    >>> table['.']
+    ['We']
+    """
+    table = {}
+    prev = '.'
+    for word in tokens:
+        if prev not in table:
+            "*** YOUR CODE HERE ***"
+        "*** YOUR CODE HERE ***"
+        prev = word
+    return table
+
+def construct_sent(word, table):
+    """Prints a random sentence starting with word, sampling from
+    table.
+
+    >>> table = {'Wow': ['!'], 'Sentences': ['are'], 'are': ['cool'], 'cool': ['.']}
+    >>> construct_sent('Wow', table)
+    'Wow!'
+    >>> construct_sent('Sentences', table)
+    'Sentences are cool.'
+    """
+    import random
+    result = ''
+    while word not in ['.', '!', '?']:
+        "*** YOUR CODE HERE ***"
+    return result.strip() + word
+
+def shakespeare_tokens(path='shakespeare.txt', url='http://composingprograms.com/shakespeare.txt'):
+    """Return the words of Shakespeare's plays as a list."""
+    import os
+    from urllib.request import urlopen
+    if os.path.exists(path):
+        return open('shakespeare.txt', encoding='ascii').read().split()
+    else:
+        shakespeare = urlopen(url)
+        return shakespeare.read().decode(encoding='ascii').split()
+
+# Uncomment the following two lines
+# tokens = shakespeare_tokens()
+# table = build_successors_table(tokens)
+
+def random_sent():
+    import random
+    return construct_sent(random.choice(table['.']), table)
+
+# Q8
+def prune_leaves(t, vals):
+    """Return a modified copy of t with all leaves that have a label
+    that appears in vals removed.  Return None if the entire tree is
+    pruned away.
+
+    >>> t = tree(2)
+    >>> print(prune_leaves(t, (1, 2)))
+    None
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    >>> print_tree(prune_leaves(numbers, (3, 4, 6, 7)))
+    1
+      2
+      3
+        5
+      6
+    """
+    "*** YOUR CODE HERE ***"
+    if is_leaf(t) and label(t) in vals:
+        return
+    new_branches = []
+    for branch in branches(t):
+        new_branch = prune_leaves(branch, vals)
+        if new_branch:
+            new_branches = new_branches + [new_branch]
+    return tree(label(t), new_branches)
+            
+
+# Q9
+def sprout_leaves(t, vals):
+    """Sprout new leaves containing the data in vals at each leaf in
+    the original tree t and return the resulting tree.
+
+    >>> t1 = tree(1, [tree(2), tree(3)])
+    >>> print_tree(t1)
+    1
+      2
+      3
+    >>> new1 = sprout_leaves(t1, [4, 5])
+    >>> print_tree(new1)
+    1
+      2
+        4
+        5
+      3
+        4
+        5
+
+    >>> t2 = tree(1, [tree(2, [tree(3)])])
+    >>> print_tree(t2)
+    1
+      2
+        3
+    >>> new2 = sprout_leaves(t2, [6, 1, 2])
+    >>> print_tree(new2)
+    1
+      2
+        3
+          6
+          1
+          2
+    """
+    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+            return tree(label(t), [tree(elem) for elem in vals])
+    new_branches = []
+    for branch in branches(t):
+        new_branch = sprout_leaves(branch, vals)
+        new_branches = new_branches + [new_branch]
+    return tree(label(t), new_branches)
+        
+
+# Q10
+def add_trees(t1, t2):
+    """
+    >>> numbers = tree(1,
+    ...                [tree(2,
+    ...                      [tree(3),
+    ...                       tree(4)]),
+    ...                 tree(5,
+    ...                      [tree(6,
+    ...                            [tree(7)]),
+    ...                       tree(8)])])
+    >>> print_tree(add_trees(numbers, numbers))
+    2
+      4
+        6
+        8
+      10
+        12
+          14
+        16
+    >>> print_tree(add_trees(tree(2), tree(3, [tree(4), tree(5)])))
+    5
+      4
+      5
+    >>> print_tree(add_trees(tree(2, [tree(3)]), tree(2, [tree(3), tree(4)])))
+    4
+      6
+      4
+    >>> print_tree(add_trees(tree(2, [tree(3, [tree(4), tree(5)])]), \
+    tree(2, [tree(3, [tree(4)]), tree(5)])))
+    4
+      6
+        8
+        5
+      5
+    """
+    "*** YOUR CODE HERE ***"
+    if not t1:
+        return t2
+    if not t2:
+        return t1
+    new_label = label(t1) + label(t2)
+    branch1, branch2 = branches(t1), branches(t2)
+    length1, length2 = len(branch1), len(branch2)
+    if length1 < length2:
+        branch1 += [None for elem in range(length1, length2)]
+    elif length1 > length2:
+        branch2 += [None for elem in range(length2, length1)]
+    return tree(new_label, [add_trees(b1, b2) for b1, b2 in zip(branch1, branch2)])
+    
